a1dea53d284a3d3cc40837e05895e466acc22707
maxplus4108 <max.plus4108@gmail.com>
2024-09-07 22:08:50 +0500

    Вроде рабочий комит 50/50

--- app/views.py
+++ app/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
 def index_page(request):
-    return render(request, 'index.html')
+    theme = request.COOKIES.get('theme', 'light')
+    language = request.COOKIES.get('language', 'ru')
+    last_team = request.COOKIES.get('team', '')
+
+    return render(request, 'index.html', {
+        'theme': theme,
+        'language': language,
+        'team': last_team
+    })
+def save_settings(request):
+    if request.method == 'POST':
+        response = redirect('index')
+        response.set_cookie('team', request.POST.get('team'))
+        response.set_cookie('theme', request.POST.get('theme'))
+        response.set_cookie('language', request.POST.get('language'))
+        return response
+    
--- maxdoc/urls.py
+++ maxdoc/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
-from app.views import index_page
+from app import views
+from django.contrib import admin
 
 urlpatterns = [
+    path('', views.index_page, name='index'),
+    path('save-settings/', views.save_settings, name='save_settings'),
     path('admin/', admin.site.urls),
-    path('', index_page)
 ]
